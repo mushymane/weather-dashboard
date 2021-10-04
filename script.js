74f8fad6dc2c89921d702179fbcd92053a13d912
Gabriel Lantin <mushymanee@gmail.com>
2021-10-03 22:15:32 -0700

    basic layout

--- script.js
+++ script.js
@@ -15,39 +15,18 @@ function init() {
     renderPrevCities();
 }
 
-function storePrev() {
-    localStorage.setItem("prevCities", JSON.stringify(prevCities));
-}
-  
-  // Add submit event to form
-  //We add an event listener to the to-do form so that when a user enters a to-do item, the data is stored on submit. The to-dos are stored and rendered using two helper functions, storeTodos() and renderTodos()
-cityForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    var cityText = cityInput.value.trim();
-  
-    // Return from function early if submitted cityText is blank
-    if (cityText === "") {
-      return;
-    }
-  
-    // Add new todoText to todos array, clear the input
-    prevCities.push(cityText);
-    cityInput.value = "";
-  
-    // Store updated todos in localStorage, re-render the list
-    storePrev();
-    renderPrevCities();
-});
-
 function renderWeather() {
     // Display todays weather in larger div
         // City name, date, icon, temperature, humidity, wind speed, uv index
     // Display 5day forcast cards with date, icon, temperature, humidity
 }
 
+function storePrev() {
+    localStorage.setItem("prevCities", JSON.stringify(prevCities));
+}
+
 function renderPrevCities() {
-    // Clear todoList element and update
+    // Clear prevButtons element and update
     prevButtonsEl.innerHTML = "";
   
     // Render a new button for each city
@@ -62,6 +41,28 @@ function renderPrevCities() {
     }
 }
 
+// Event listener for submitted city input
+cityForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var cityText = cityInput.value.trim();
+  
+    // Return from function early if submitted cityText is blank
+    if (cityText === "") {
+      return;
+    }
+  
+    // Add new cityText to todos array, clear the input
+    prevCities.push(cityText);
+    cityInput.value = "";
+  
+    // Store updated cities in localStorage, re-render the list, render the weather
+    storePrev();
+    renderPrevCities();
+    renderWeather();
+});
+
+// Event listener for previous cities buttons
 prevButtonsEl.addEventListener("click", function(event) {
     var element = event.target;
     console.log("I was clicked");
