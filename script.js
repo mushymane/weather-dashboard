f3ba60cf0818dc6874cfc79b20fb335435dae0d7
Gabriel Lantin <mushymanee@gmail.com>
2021-10-03 17:04:25 -0700

    added working previous cities - need to add functionality where it display weather when clicked

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 var prevButtonsEl = document.querySelector('#prev-buttons');
 var cityInput = document.querySelector("#city-text");
 var cityForm = document.querySelector("#city-form");
+var button = document.querySelector(".btn");
 
 var prevCities = [];
 
 function init() {
-    // Get stored todos from localStorage
-    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+    var storedCities = JSON.parse(localStorage.getItem("prevCities"));
   
-    // If todos were retrieved from localStorage, update the todos array to it
     if (storedTodos !== null) {
-      todos = storedTodos;
+      prevCities = storedCities;
     }
   
-    // This is a helper function that will render todos to the DOM
-    renderTodos();
+    renderPrevCities();
 }
 
 function storePrev() {
@@ -23,29 +21,45 @@ function storePrev() {
   
   // Add submit event to form
   //We add an event listener to the to-do form so that when a user enters a to-do item, the data is stored on submit. The to-dos are stored and rendered using two helper functions, storeTodos() and renderTodos()
-todoForm.addEventListener("submit", function(event) {
+cityForm.addEventListener("submit", function(event) {
     event.preventDefault();
   
-    var todoText = todoInput.value.trim();
+    var cityText = cityInput.value.trim();
   
-    // Return from function early if submitted todoText is blank
-    if (todoText === "") {
+    // Return from function early if submitted cityText is blank
+    if (cityText === "") {
       return;
     }
   
     // Add new todoText to todos array, clear the input
-    todos.push(todoText);
-    todoInput.value = "";
+    prevCities.push(cityText);
+    cityInput.value = "";
   
     // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
+    storePrev();
+    renderPrevCities();
 });
 
 function renderWeather() {
     // Display todays weather in larger div
         // City name, date, icon, temperature, humidity, wind speed, uv index
     // Display 5day forcast cards with date, icon, temperature, humidity
+}
+
+function renderPrevCities() {
+    // Clear todoList element and update
+    prevButtonsEl.innerHTML = "";
+  
+    // Render a new button for each city
+    for (var i = prevCities.length - 1; i >= 0; i--) {
+      var city = prevCities[i];
+  
+      var btn = document.createElement("button");
+      btn.textContent = city;
+
+      btn.setAttribute("class", "btn");
+      prevButtonsEl.appendChild(btn);
+    }
 }
 
 prevButtonsEl.addEventListener("click", function(event) {
